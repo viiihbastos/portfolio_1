adc611dfe8cb47c9b44995580d694211c2b4bf14
Victor Bastos <victorbastos26@hotmail.com>
2025-05-26 21:10:42 +0100

    Please provide the file changes you'd like me to summarize in a commit message.

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: [
-        "hebbkx1anhila5yf.public.blob.vercel-storage.com",
-      ],
-    },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: [
+      "hebbkx1anhila5yf.public.blob.vercel-storage.com",
+    ],
+  },
 };
 
 export default nextConfig;
