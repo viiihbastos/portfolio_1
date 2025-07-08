cc1eea42bfa98d477ec3e9f12c2e133f2c626e42
Victor Bastos <victorbastos26@hotmail.com>
2025-07-08 23:31:28 +0100

    Atualização do portfolio

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  distDir: 'build', // Define o diretório de saída como 'build'
+  output: 'export', // Exporta como arquivos estáticos
+}
+
+module.exports = nextConfig
